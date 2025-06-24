1178f41453ed45104c383380477bbc1ab65850a8
juankOrtiz <juankortiz@proton.me>
2025-06-24 17:22:29 -0300

    Creacion del proyecto de JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.juankortiz.introduccionjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.juankortiz.introduccionjavafx to javafx.fxml;
+    exports com.juankortiz.introduccionjavafx;
+
+    opens com.juankortiz.introduccionjavafx.dashboard to javafx.fxml;
+    exports com.juankortiz.introduccionjavafx.dashboard;
+
+    opens com.juankortiz.introduccionjavafx.cambioescena to javafx.fxml;
+    exports com.juankortiz.introduccionjavafx.cambioescena;
+}
